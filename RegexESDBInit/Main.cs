99cab8157f1cb369203dae1128a99f78bb7cde42
Andrii Grondzal <andriigrondzal@gmail.com>
2015-10-13 20:23:20 +0300

    move to MetroUI. Add Services project. Update UI

--- RegexESDBInit/Main.cs
+++ RegexESDBInit/Main.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MetroFramework.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,16 @@ using System.Windows.Forms;
 
 namespace RegexESDBInit
 {
-    public partial class Form1 : Form
+    public partial class Main : MetroForm
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
+
+        private void btn_LoadFile_Click(object sender, EventArgs e)
+        {
+
+        }        
     }
 }
