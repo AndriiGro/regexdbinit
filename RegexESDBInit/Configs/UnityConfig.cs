ca5b54242ae19fa250f6fa1c5215019f5f348221
Andrii Grondzal <andriigrondzal@gmail.com>
2015-10-13 21:26:43 +0300

    add Service project. install Unity

---
+++ RegexESDBInit/Configs/UnityConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grondzal.RegexESDBInit.Configs
+{
+    public static class UnityConfig
+    {
+        private static UnityContainer _conteiner { get; set; }
+
+        private void SetDependencies(UnityContainer container)
+        {
+           
+        }
+    }
+}
--- RegexESDBInit/Main.cs
+++ RegexESDBInit/Main.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace RegexESDBInit
+namespace Grondzal.RegexESDBInit
 {
     public partial class Main : MetroForm
     {
